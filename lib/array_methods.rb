c9f60dcdcf6d9d641b04463f25b38b5b963fbf0b
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 00:07:23 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def using_include(array, element)
-  
+   using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
+  famous_cats.include?("wow")
+  => true
+famous_cats.include?("Tardis")
+  => false
 end
 
 def using_sort(array)
